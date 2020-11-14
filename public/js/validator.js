4a5b4b2ad232d02e7593f7a582605d11bcdbe28a
Maria Claudia <mariacladiaperezes@gmail.com>
2020-11-14 03:16:10 -0300

    se modifico las imagenes de los proyectos

--- public/js/validator.js
+++ public/js/validator.js
@@ -23,7 +23,7 @@ class Validator {
       let val = e.value;
 
       if (!this['validate'+ v.type](val, v.parameters)) {
-        errorMessages.push(`El contenido del campo <span class="fillname">'${v.fillname}'</span> con el valor '${val== "" ? val ='<span class="error-value">vacio</span>' : val}', es <span class="incorrect">incorrecto.</span>`);
+        errorMessages.push(`El contenido del campo <span class="fillname">'${v.fillname}'</span> con el valor <span class="error-value">'${val== "" ? val ='vacio' : val}'</span>, es <span class="incorrect">incorrecto.</span>`);
         
         if (this.errorClass !=='') {
           e.classList.add(this.errorClass);
@@ -31,7 +31,7 @@ class Validator {
           e.onkeyup = e.onchange;
         }
       }else{
-        correctValue.push({fillname: v.fillname, name: e.name, value: val   })
+        correctValue.push({fillname: v.fillname, name: e.name, value: val })
         console.log(val);
       }
     });
